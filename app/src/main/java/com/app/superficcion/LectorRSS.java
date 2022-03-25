7475614b6eb1396baeb8d3cdd48fe49c1726638c
Sykda <robermontes93@gmail.com>
2022-03-25 13:50:25 +0100

    XML Parseado para que muestre los elementos por pantalla.

--- app/src/main/java/com/app/superficcion/LectorRSS.java
+++ app/src/main/java/com/app/superficcion/LectorRSS.java
@@ -1,6 +1,5 @@
 package com.app.superficcion;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
@@ -8,6 +7,9 @@ import android.view.View;
 import android.widget.ProgressBar;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -17,29 +19,29 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 //AsincTask crea un hilo nuevo.
-public class LectorRSS extends AsyncTask<Void, Void, Void>{
+public class LectorRSS extends AsyncTask<Void, Void, Void> {
 
     private Context context;
-    String direccion= "https://super-ficcion.com/feed/";
+    String direccion = "https://super-ficcion.com/feed/";
     URL url;
     private ProgressBar progressBar;
 
     public LectorRSS(Context context) {
-        this.context=context;
-        progressBar =  new ProgressBar(context);
+        this.context = context;
+        progressBar = new ProgressBar(context);
 
     }
 
     @Override
-    protected void onPreExecute(){
+    protected void onPreExecute() {
         progressBar.setVisibility(View.VISIBLE);
-        progressBar.setProgress(0);
         super.onPreExecute();
     }
 
     @Override
-    protected void onPostExecute(Void aVoid){
+    protected void onPostExecute(Void aVoid) {
         super.onPostExecute(aVoid);
+        progressBar.setVisibility(View.GONE);
     }
 
     @Override
@@ -48,25 +50,38 @@ public class LectorRSS extends AsyncTask<Void, Void, Void>{
         return null;
     }
 
-    private void procesarXML(Document data){
+    private void procesarXML(Document data) {
         //si hay doc me devuelve el log
-            if(data != null){
-                Log.d("Elemento root",data.getDocumentElement().getNodeName());
+        if (data != null) {
+            Element root = data.getDocumentElement();
+            Node channel = root.getChildNodes().item(1);
+            NodeList items = channel.getChildNodes();
+            for (int i = 0; i < items.getLength(); i++) {
+                Node hijoActual = items.item(i);
+                if (hijoActual.getNodeName().equalsIgnoreCase("item")) {
+
+                    NodeList itemChilds = hijoActual.getChildNodes();
+                    for (int j = 0; j < itemChilds.getLength(); j++) {
+                        Node actual = itemChilds.item(j);
+                        Log.d("Elementos: ",actual.getTextContent());
+                    }
+                }
             }
+        }
     }
 
-    public Document obtenerDatos (){
-        try{
+    public Document obtenerDatos() {
+        try {
             //Peticion a la URL, leemos  la peticion la procesamos y la guardamos en un doc.
             url = new URL(direccion);
-            HttpURLConnection connection=(HttpURLConnection) url.openConnection();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");//tipo de petición
             InputStream inputStream = connection.getInputStream();
-            DocumentBuilderFactory builderFactory=DocumentBuilderFactory.newInstance();
+            DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = builderFactory.newDocumentBuilder();
             Document xmlDoc = builder.parse(inputStream); //Parsear
             return xmlDoc;
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
