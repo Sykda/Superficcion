b921a5e2633a91999ab780b6f27a202baeeb9892
Sykda <robermontes93@gmail.com>
2022-04-06 10:56:16 +0200

    Agregada la ActionBar personalizada

--- app/src/main/java/com/app/superficcion/MainActivity.java
+++ app/src/main/java/com/app/superficcion/MainActivity.java
@@ -1,25 +1,31 @@
 package com.app.superficcion;
 
+import android.os.Bundle;
+import android.widget.Toolbar;
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM); getSupportActionBar().setCustomView(R.layout.abs_layout);
+        toolbar=findViewById(R.id.toolbar);
 
-        recyclerView=findViewById(R.id.recycleView);
 
-        LectorRSS lectorRSS = new LectorRSS(getApplicationContext(),recyclerView);
+        recyclerView = findViewById(R.id.recycleView);
+
+
+
+        LectorRSS lectorRSS = new LectorRSS(getApplicationContext(), recyclerView);
         lectorRSS.execute(); // Lanzamos el hilo.
 
 
